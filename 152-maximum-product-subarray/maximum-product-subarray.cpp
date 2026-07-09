e91b3653c2a6f524af63c71bf97a4b113543cb6b
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-07-09 16:43:07 +0530

    Time: 303 ms (7.69%) | Memory: 17.9 MB (23.32%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n= nums.size();
-        int currMax=nums[0];
-        int currMin=nums[0];
-        int ans=nums[0];
-        for(int i=1; i<n; i++) {
-            if(nums[i]<0) {
-                swap(currMax, currMin);
-            }
-            currMax=max(nums[i], currMax*nums[i]);
-            currMin=min(nums[i], currMin*nums[i]);
+        // int n= nums.size();
+        // int currMax=nums[0];
+        // int currMin=nums[0];
+        // int ans=nums[0];
+        // for(int i=1; i<n; i++) {
+        //     if(nums[i]<0) {
+        //         swap(currMax, currMin);
+        //     }
+        //     currMax=max(nums[i], currMax*nums[i]);
+        //     currMin=min(nums[i], currMin*nums[i]);
 
-            ans=max(ans, currMax);
+        //     ans=max(ans, currMax);
+        // }
+        // return ans;
+        int n=nums.size();
+
+
+        int max_pro=nums[0];
+        for(int i=0; i<n; i++) {
+            int product=1;
+            for(int j=i; j<n; j++) {
+                product*=nums[j];
+                max_pro=max(max_pro, product);
+            }
+            
         }
-        return ans;
+        return max_pro;
     }
 };
